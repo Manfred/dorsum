08c4968025039b41920ea13b34991bac4df68d2d
Manfred Stienstra <manfred@fngtps.com>
2022-01-21 09:05:24 +0100

    Attempt to make socket initialization more reliable.

--- src/dorsum/client.cr
+++ src/dorsum/client.cr
@@ -13,7 +13,10 @@ module Dorsum
       tcp_socket.read_timeout = 15
       tcp_socket.write_timeout = 5
       tcp_socket.keepalive = true
+      Log.info { "Connecting to #{HOST}…" }
       @socket = OpenSSL::SSL::Socket::Client.new(tcp_socket)
+      # Prevent other code from using the socket before it established completely by interacting with it.
+      @socket.as(OpenSSL::SSL::Socket::Client).unbuffered_flush
       self
     end
 
